c8d679c2d92d546f364d507c2c17021305fb79dc
qiang.wang <qiang.wang@ersoft.io>
2020-04-13 16:06:34 +0800

    从数据库表生成Excel

--- 0-Code/server/member/admin/src/main/java/com/wq/member/service/impl/UserServiceImpl.java
+++ 0-Code/server/member/admin/src/main/java/com/wq/member/service/impl/UserServiceImpl.java
@@ -2,9 +2,15 @@ package com.wq.member.service.impl;
 
 import com.wq.member.mapper.UserMapper;
 import com.wq.member.model.User;
+import com.wq.member.model.UserExample;
 import com.wq.member.service.UserService;
+import com.wq.member.util.CreatExcel;
+import com.wq.member.util.MapAndBeanUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -17,5 +23,16 @@ public class UserServiceImpl implements UserService {
         
         userMapper.insert(user);
 
+    }
+
+    @Override
+    public void userExcel() {
+        List<User> userList = userMapper.selectByExample(new UserExample());
+        List<Map<String, Object>> mapList = MapAndBeanUtil.beansToMap(userList);
+        try {
+            CreatExcel.writeExcel(mapList,"/Users/wq/Java-Platform");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
