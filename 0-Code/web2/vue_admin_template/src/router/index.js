13aaa51eb957b09f46bbd881a2dc20c23ba968f7
qiang.wang <qiang.wang@ersoft.io>
2020-05-09 11:31:39 +0800

    在新的vue前端框架下完成添加会员功能

--- 0-Code/web2/vue_admin_template/src/router/index.js
+++ 0-Code/web2/vue_admin_template/src/router/index.js
@@ -3,25 +3,31 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-/* Layout */
+/* Layout 布局 */
 import Layout from '@/layout'
 
 /**
- * Note: sub-menu only appear when route children.length >= 1
- * Detail see: https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
+ * 注意：子菜单仅在路由 children.length > = 1 时出现
+ * 详细信息请参见：https://panjiachen.gitee.io/vue-element-admin-site/zh/guide/essentials/router-and-nav.html#%E9%85%8D%E7%BD%AE%E9%A1%B9
  *
- * hidden: true                   if set true, item will not show in the sidebar(default is false)
- * alwaysShow: true               if set true, will always show the root menu
- *                                if not set alwaysShow, when item has more than one children route,
- *                                it will becomes nested mode, otherwise not show the root menu
- * redirect: noRedirect           if set noRedirect will no redirect in the breadcrumb
+ * hidden: true                   // 当设置 true 的时候该路由不会在侧边栏出现 如401，login等页面，或者如一些编辑页面/edit/1 (default is false)
+ * alwaysShow: true               // 当你一个路由下面的 children 声明的路由大于1个时，自动会变成嵌套的模式--如组件页面
+                                  // 只有一个时，会将那个子路由当做根路由显示在侧边栏--如引导页面
+                                  // 若你想不管路由下面的 children 声明的个数都显示你的根路由
+                                  // 你可以设置 alwaysShow: true，这样它就会忽略之前定义的规则，一直显示根路由
+
+ * redirect: 'noRedirect'          //当设置 noRedirect 的时候该路由在面包屑导航中不可被点击
+
+ // 设定路由的名字，一定要填写不然使用<keep-alive>时会出现各种问题
  * name:'router-name'             the name is used by <keep-alive> (must set!!!)
+
+
  * meta : {
-    roles: ['admin','editor']    control the page roles (you can set multiple roles)
-    title: 'title'               the name show in sidebar and breadcrumb (recommend set)
+    roles: ['admin','editor']    // 设置该路由进入的权限，支持多个权限叠加
+    title: 'title'               // 设置该路由在侧边栏和面包屑中展示的名字
     icon: 'svg-name'             the icon show in the sidebar
-    breadcrumb: false            if set false, the item will hidden in breadcrumb(default is true)
-    activeMenu: '/example/list'  if set path, the sidebar will highlight the path you set
+    breadcrumb: false            //  如果设置为false，则不会在breadcrumb面包屑中显示(默认 true)
+    activeMenu: '/example/list'  // 高亮
   }
  */
 
@@ -47,12 +53,35 @@ export const constantRoutes = [
     path: '/',
     component: Layout,
     redirect: '/dashboard',
-    children: [{
-      path: 'dashboard',
-      name: 'Dashboard',
-      component: () => import('@/views/dashboard/index'),
-      meta: { title: 'Dashboard', icon: 'dashboard' }
-    }]
+    children: [
+      {
+        path: 'dashboard',
+        name: 'Dashboard',
+        component: () => import('@/views/dashboard/index'),
+        meta: { title: 'Dashboard', icon: 'dashboard' }
+      },
+      {
+        path: 'addmember',
+        name: 'addmember',
+        hidden: true,
+        component: () => import('@/views/member/AddMember'), // 添加会员页面
+        meta: { title: 'addmember', icon: 'lock' }
+      },
+      {
+        path: 'updatemember',
+        name: 'updatemember',
+        hidden: true,
+        component: () => import('@/views/member/UpdateMember'), // 更新会员页面
+        meta: { title: 'updatemember', icon: 'lock' }
+      },
+      {
+        path: 'memberdetail',
+        name: 'memberdetail',
+        hidden: true,
+        component: () => import('@/views/member/components/MemberDetail'), // 会员页面详情
+        meta: { title: 'memberdetail', icon: 'lock' }
+      }
+    ]
   },
 
   {
@@ -153,7 +182,7 @@ export const constantRoutes = [
     component: Layout,
     children: [
       {
-        path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
+        path: 'https://panjiachen.gitee.io/vue-element-admin-site/zh/guide',
         meta: { title: 'External Link', icon: 'link' }
       }
     ]
@@ -163,32 +192,38 @@ export const constantRoutes = [
     // 显示在地址栏里的
     path: '/zijiwan',
     component: Layout,
+    // 面包屑导航中可被点击，点击后直接跳到对应的路径
     redirect: '/zijiwan',
-    // name没啥用啊？
+    // 面包屑导航中不可被点击
+    // redirect: 'noRedirect',
+
+    // name没啥用啊？ 被用在<keep-alive>里面
     name: 'ZIJIWAN001',
-    // 标题和图标
-    meta: { title: 'ZIJIWAN', icon: 'example' },
+    // title 显示在侧边栏里面和面包屑里面
+    meta: { title: 'ZIJIWANwan', icon: 'example' },
     children: [
       {
         path: 'minio',
         name: 'minio',
         component: () => import('@/views/minio/minio'),
-        meta: { title: 'minio', icon: 'table' }
+        meta: { title: 'minio', icon: 'table' },
+        // activeMenu: '/nested/menu1' 没看出来哪有高亮
       },
       {
         path: 'minio2',
         name: 'minio2',
-        component: () => import('@/views/minio/minio'),
-        meta: { title: 'minio2', icon: 'table' }
-      }
-  
+        component: () => import('@/views/minio/minio2'), // 空白页面
+        meta: { title: 'minio2', icon: 'lock' }
+      },
+
+
     ]
   },
 
-
-
   // 404 page must be placed at the end !!!
   { path: '*', redirect: '/404', hidden: true }
+
+
 ]
 
 const createRouter = () => new Router({
