8349b0c893f44e28e5b777ea216fd3a32426feef
qiang.wang <qiang.wang@ersoft.io>
2020-07-14 10:50:41 +0800

    feat：配置

--- 0-Code/server/member/README.md
+++ 0-Code/server/member/README.md
@@ -1,2 +1,19 @@
 # member
 会员管理系统
+
+本系统user就代表admin，即用户就是管理员
+完成情况：
+1、在需要权限的接口上使用@PreAuthorize注解定义好需要的权限。已完成
+    @ResponseBody
+    @GetMapping("/list")
+    @PreAuthorize("hasAuthority('member:list:read')")
+    public CommonResult memberList(){
+        System.out.println("进入memberList方法了");
+        List<Member> memberList = memberService.list();
+        return CommonResult.success(memberList);
+    }
+这样需要在每个接口上都定义好访问该接口的权限值
+
+2、打算写动态权限配置
+
+
--- 0-Code/server/member/SQLscript/role.sql
+++ 0-Code/server/member/SQLscript/role.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80019
  File Encoding         : 65001
 
- Date: 10/05/2020 12:58:26
+ Date: 13/07/2020 11:23:19
 */
 
 SET NAMES utf8mb4;
@@ -23,10 +23,13 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `role`;
 CREATE TABLE `role` (
   `id` int NOT NULL AUTO_INCREMENT COMMENT '角色id',
-  `role_name` varchar(255) DEFAULT NULL COMMENT '角色名称',
-  `authority_id` varchar(0) DEFAULT NULL COMMENT '权限id',
-  `user_id` varchar(0) DEFAULT NULL COMMENT '人员id',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '角色名称',
+  `description` varchar(255) DEFAULT NULL COMMENT '对角色的描述',
+  `user_count` varchar(255) DEFAULT NULL COMMENT '用户数量',
+  `create_time` timestamp NULL DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL COMMENT '状态',
+  `sort` int DEFAULT NULL COMMENT '排序',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 SET FOREIGN_KEY_CHECKS = 1;
